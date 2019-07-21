af0638f76a16e29e147d5c00b8362e843645385e
Yingjie Guo <guoyj@live.com>
2019-07-21 16:00:07 +0000

    change default cursor to 0

--- services/message.js
+++ services/message.js
@@ -1,9 +1,9 @@
 const { Messages, Sequelize } = require('../db/models');
 
-function getMessages(cursor = -1, previous = false, limit = 10) {
+function getMessages(cursor = 0, previous = false, limit = 10) {
   const where = {};
   let order = [['id', 'DESC']];
-  if (cursor !== -1) {
+  if (cursor !== 0) {
     where['id'] = previous
       ? { [Sequelize.Op.lt]: cursor }
       : { [Sequelize.Op.gt]: cursor };
