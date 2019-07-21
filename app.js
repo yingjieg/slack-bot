1a2c4f2b209df344adbe96f62e284db2ce6ea5d3
Yingjie Guo <yingjie.guo@axesdn.com>
2019-07-21 10:46:40 +0800

    add view engine for express

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const path = require('path');
+const createError = require('http-errors');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+require('dotenv').config();
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const cmdRouter = require('./routes/cmds');
+const msgRouter = require('./routes/messages');
+const userProfileRouter = require('./routes/user-profiles');
 
 const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -14,7 +21,30 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/cmds', cmdRouter);
+app.use('/api/messages', msgRouter);
+app.use('/api/user-profiles', userProfileRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+// customer event handlers
+const eventEmitter = require('./events');
+const { notifyHandler } = require('./events/slack');
+
+eventEmitter.on('slack-notify', notifyHandler);
 
 module.exports = app;
